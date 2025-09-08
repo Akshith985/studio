f3b968083b0b4ea7e09c5b03d9e7016530fce11b
Akshith Anand <akshithanand86@gmail.com>
2025-09-08 15:13:52 +0000

    done both

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  projectId: "mindful-moment-mb8na",
-  appId: "1:938749099905:web:8629b03379e549fc73f280",
-  storageBucket: "mindful-moment-mb8na.firebasestorage.app",
-  apiKey: "AIzaSyBRoGJ5-6DTXClGVvPjNX_GFyqLE0-1Dlg",
-  authDomain: "mindful-moment-mb8na.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "938749099905"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
